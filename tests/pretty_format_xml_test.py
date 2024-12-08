3e4b9fef931d4830efee4597ca0fd9f805b23899
Matthieu Bizien <matthieu.bizien@polytechnique.org>
2024-12-08 12:39:56 +0100

    Add test files

--- tests/pretty_format_xml_test.py
+++ tests/pretty_format_xml_test.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os
+import shutil
+
 import pytest
 
 from pre_commit_hooks.pretty_format_xml import main
@@ -90,9 +93,83 @@ def test_doctype():
 
 @pytest.mark.parametrize(
     ('filename', 'expected_retval'), (
+        ('not_pretty_formatted_xml.xml', 1),
         ('pretty_formatted_xml.xml', 0),
     ),
 )
 def test_unsorted_main(filename, expected_retval):
     ret = main([get_resource_path(filename)])
     assert ret == expected_retval
+
+
+@pytest.mark.parametrize(
+    ('filename', 'expected_retval'), (
+        ('not_pretty_formatted_xml.xml', 1),
+        ('pretty_formatted_xml.xml', 1),
+        ('tab_pretty_formatted_xml.xml', 0),
+    ),
+)
+def test_tab_main(filename, expected_retval):
+    ret = main(['--indent', '\t', get_resource_path(filename)])
+    assert ret == expected_retval
+
+
+def test_autofix_main(tmpdir):
+    srcfile = tmpdir.join('to_be_xml_formatted.xml')
+    shutil.copyfile(
+        get_resource_path('not_pretty_formatted_xml.xml'),
+        str(srcfile),
+    )
+
+    # now launch the autofix on that file
+    ret = main(['--autofix', str(srcfile)])
+    # it should have formatted it
+    assert ret == 1
+
+    # file was formatted (shouldn't trigger linter again)
+    ret = main([str(srcfile)])
+    assert ret == 0
+
+
+def test_invalid_main(tmpdir):
+    srcfile1 = tmpdir.join('not_valid_xml.xml')
+    srcfile1.write('<This> is not valid xml')
+    srcfile2 = tmpdir.join('to_be_xml_formatted.xml')
+    shutil.copyfile(
+        get_resource_path('not_pretty_formatted_xml.xml'),
+        str(srcfile2),
+    )
+
+    # it should have skipped the first file and formatted the second one
+    assert main(['--autofix', str(srcfile1), str(srcfile2)]) == 1
+
+    # confirm second file was formatted (shouldn't trigger linter again)
+    assert main([str(srcfile2)]) == 0
+
+
+@pytest.mark.xfail
+def test_badfile_main():
+    ret = main([get_resource_path('ok_yaml.yaml')])
+    assert ret == 1
+
+
+def test_diffing_output(capsys):
+    resource_path = get_resource_path('not_pretty_formatted_xml.xml')
+    expected_retval = 1
+    a = os.path.join('a', resource_path)
+    b = os.path.join('b', resource_path)
+    expected_out = f'''\
+--- {a}
++++ {b}
+@@ -1,2 +1,3 @@
+-<hello><world>!</world>
++<hello>
++    <world>!</world>
+ </hello>
+'''
+    actual_retval = main([resource_path])
+    actual_out, actual_err = capsys.readouterr()
+
+    assert actual_retval == expected_retval
+    assert actual_out == expected_out
+    assert actual_err == ''
